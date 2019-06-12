a84a69ba2f9ac569184d484cc11ee02f4c2e08ef
Steve <steve.liu.3029@gmail.com>
2019-06-12 13:44:56 +1200

    implement EditProfile component and refactor DynamicForm component to make it fit to profile form

--- src/DevYeah.LMS/DevYeah.LMS.Web/app/src/account/EditProfile.js
+++ src/DevYeah.LMS/DevYeah.LMS.Web/app/src/account/EditProfile.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DynamicForm from './DynamicForm';
+import { formMeta, formValidation} from './formMeta/profileFormMeta';
 
-export default function EditProfile() {
-  return (
-    <div>
-      Edit Profile
-    </div>
-  );
+export default class EditProfile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  submitHandler(values, actions) {
+    //todo: submit values to backend server
+    actions.setSubmitting(false);
+    actions.resetForm();
+  }
+
+  render() {
+    return (
+      <div className="card">
+        <div className="card-header">
+          Profile details
+        </div>
+        <div className="card-body">
+          <DynamicForm 
+            isEmbedded
+            formName="profileForm"
+            submitHandler={this.submitHandler}
+            formValidation={formValidation}
+            formMeta={formMeta}
+          />
+        </div>
+      </div>
+    );
+  }
+  
 }
