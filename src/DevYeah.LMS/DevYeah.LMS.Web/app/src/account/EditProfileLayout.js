68c66406415e8669c7f0e06769e2bf3d6963e46b
Steve <steve.liu.3029@gmail.com>
2019-06-12 12:17:59 +1200

    add PhotoUpload component for editting user avatar

--- src/DevYeah.LMS/DevYeah.LMS.Web/app/src/account/EditProfileLayout.js
+++ src/DevYeah.LMS/DevYeah.LMS.Web/app/src/account/EditProfileLayout.js
@@ -1,35 +1,44 @@
 import React, { useState } from 'react';
 import { Route, Link } from 'react-router-dom';
-import EditPhoto from './EditPhoto';
+import PhotoUpload from './PhotoUpload';
 import editProfile from './EditProfile';
 import './account.css';
 
 export default function EditProfileLayout() {
-  const [activeMenu, setActiveMenu] = useState('profile');
+  const [activeMenu, setActiveMenu] = useState();
 
   return (
     <div id="profileContainer" className="container">
       <div className="row mt-4">
         <div id="menus" className="col-3 list-group">
-          <Link 
-            id="editProfile"
-            className={"list-group-item list-group-item-action " + (activeMenu==="profile" ? "active" : "")}
-            onClick={() => setActiveMenu('profile')}
-            to="/account/editProfile/profile"
-          >
-            Edit Profile
-          </Link>
+          <div className="list-group">
+            <Link 
+              id="editProfile"
+              className={"list-group-item list-group-item-action " + (activeMenu==="profile" ? "active" : "")}
+              onClick={() => setActiveMenu('profile')}
+              to="/account/editProfile/profile"
+            >
+              Edit Profile
+            </Link>
+            <Link
+              id="uploadPhoto"
+              className={"list-group-item list-group-item-action " + (activeMenu==="photo" ? "active" : "")}
+              onClick={() => setActiveMenu('photo')}
+              to="/account/editProfile/photo" 
+            >
+              Photo
+            </Link>
+          </div>
           <Link
-            id="uploadPhoto"
-            className={"list-group-item list-group-item-action " + (activeMenu==="photo" ? "active" : "")}
-            onClick={() => setActiveMenu('photo')}
-            to="/account/editProfile/photo" 
+            id="viewProfileBtn"
+            className="btn btn-outline-secondary font-weight-bold mt-3"
+            to="/account/viewProfile"
           >
-            Photo
+            View Profile
           </Link>
         </div>
         <div id="content" className="col-8">
-          <Route path="/account/editProfile/photo" component={EditPhoto} />
+          <Route path="/account/editProfile/photo" component={PhotoUpload} />
           <Route path="/account/editProfile/profile" component={editProfile} />
         </div>
       </div>
