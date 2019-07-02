7b7fa31f87da085a3979042f95c3f360390a0ab2
Steve <steve.liu.3029@gmail.com>
2019-07-02 15:46:11 +1200

    refactor DropDownMenu component

--- src/DevYeah.LMS/DevYeah.LMS.Web/app/src/header/DropDownMenu.js
+++ src/DevYeah.LMS/DevYeah.LMS.Web/app/src/header/DropDownMenu.js
@@ -1,60 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signout } from '../account/redux/actions';
 
-class DropDownMenu extends Component {
+function DropDownMenu({ signout, account }) {
 
-  constructor(props) {
-    super(props);
-
-    this.accountSignOut = this.accountSignOut.bind(this);
-  }
-
-  accountSignOut() {
-    this.props.signout();
-  }
-
-  render() {
-    const { account } = this.props;
-    return (
-      <div className="dropdown">
-        <button 
-          className="btn btn-outline-success dropdown-toggle"
-          type="button"
-          id="dropDownMenuBtn"
-          data-toggle="dropdown"
-          aria-haspopup="true" 
-          aria-expanded="false"
+  return (
+    <div className="dropdown">
+      <button 
+        className="btn btn-outline-success dropdown-toggle"
+        type="button"
+        id="dropDownMenuBtn"
+        data-toggle="dropdown"
+        aria-haspopup="true" 
+        aria-expanded="false"
+      >
+        <span id="navbarUserMenu">
+          { `Welcome, ${account.username}` }
+        </span>
+      </button>
+      <div 
+        className="dropdown-menu" 
+        aria-labelledby="dropDownMenuBtn"
+      >
+        <Link 
+          className="dropdown-item" 
+          to="/account/editProfile"
+          id="profileDropDownMenu"
         >
-          <span id="navbarUserMenu">
-            { `Welcome, ${account.username}` }
-          </span>
-        </button>
-        <div 
-          className="dropdown-menu" 
-          aria-labelledby="dropDownMenuBtn"
+          Profile
+        </Link>
+        <Link 
+          className="dropdown-item"
+          id="logoutMenu"
+          to="/"
+          onClick={() => (signout())}
         >
-          <Link 
-            className="dropdown-item" 
-            to="/account/editProfile"
-            id="profileDropDownMenu"
-          >
-            Profile
-          </Link>
-          <Link 
-            className="dropdown-item"
-            id="logoutMenu"
-            to="/"
-            onClick={this.accountSignOut}
-          >
-            Log out
-          </Link>
-        </div>
+          Log out
+        </Link>
       </div>
-    );
-  }
-  
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
