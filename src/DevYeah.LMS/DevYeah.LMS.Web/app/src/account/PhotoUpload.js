b7ec1e390c590f8c357087ebd3d62bf3f81bef9a
Steve <steve.liu.3029@gmail.com>
2019-07-12 10:08:31 +1200

    add preview for avatar edition

--- src/DevYeah.LMS/DevYeah.LMS.Web/app/src/account/PhotoUpload.js
+++ src/DevYeah.LMS/DevYeah.LMS.Web/app/src/account/PhotoUpload.js
@@ -1,62 +1,71 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDropzone } from 'react-dropzone';
 import defaultAvatar from '../images/user_pic-225x225.png';
 import './account.css';
 
-export default class PhotoUpload extends Component {
-  constructor(props) {
-    super(props);
+export default function PhotoUpload() {
+  const [images, setImages] = useState([]);
+  const {getRootProps, getInputProps} = useDropzone({
+    accept: 'image/*',
+    onDrop: acceptedImages => {
+      setImages(acceptedImages.map(image => Object.assign(image, {
+        preview: URL.createObjectURL(image),
+      })));
+    }
+  });
 
-    this.state = {avatarImg: null}
-    this.fileInputRef = React.createRef();
-    this.openFileDialog = this.openFileDialog.bind(this);
-  }
+  useEffect(() => () => {
+    images.forEach(image => URL.revokeObjectURL(image.preview));
+  }, [images]);
 
-  openFileDialog() {
-    this.fileInputRef.current.click();
-  }
+  const thumbs = images.map(image => (
+    <img 
+      key={image.name}
+      alt="avatar"
+      title="avatar"
+      className="avatar"
+      src={image.preview}
+    />
+  ));
 
-  render() {
-    return (
-      <div className="card">
-        <div className="card-header font-weight-bold">
-          Profile Photo
-        </div>
-        <div className="card-body container">
-          <div className="row">
-            <div className="col-4">
-              <img 
-                alt="avatar"
-                title="avatar"
-                className="avatar rounded-circle"
-                src={this.state.avatarImg ? this.state.avatarImg : defaultAvatar}
-              />
-            </div>
-            <div className="col-8">
-              <p>
-                Clear frontal face photos are an important way for hosts and guests to learn about each other. 
-                It’s not much fun to host a landscape! Be sure to use a photo that clearly shows your face and 
-                doesn’t include any personal or sensitive info you’d rather not have hosts or guests see.
-              </p>
-              <button 
-                id="avatarUploadBtn"
-                type="button"
-                className="btn btn-outline-secondary font-weight-bold btn-block"
-                onClick={this.openFileDialog}
-              >
-                Upload a file from your computer
+  return (
+    <div className="card">
+      <div className="card-header font-weight-bold">
+        Profile Photo
+      </div>
+      <div className="card-body container">
+        <div className="row">
+          <div className="col-4">
+            {(images.length > 0)
+              ? thumbs
+              : (
+                <img 
+                  alt="avatar"
+                  title="avatar"
+                  className="avatar"
+                  src={defaultAvatar}
+                />
+              )
+            }
+            <div className="mt-2">
+              <button className="btn btn-danger btn-block font-weight-bold">
+                save
               </button>
-              <input 
-                ref={this.fileInputRef}
-                id="userAvatarPic" 
-                type="file" 
-                accept="image/*" 
-                hidden 
-              />
+            </div>
+          </div>
+          <div className="col-8">
+            <p id="tipsForUploadAvatar">
+              Clear frontal face photos are an important way for other people to learn about you. 
+              It’s not much fun to host a landscape! Be sure to use a photo that clearly shows your face and 
+              doesn’t include any personal or sensitive info you’d rather not have others see.
+            </p>
+            <div {...getRootProps({className: 'dropzone'})}>
+              <input {...getInputProps()} />
+              <p id="avatarUploadPlaceHolder">Drag 'n' drop your photo here, or click to select files</p>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
 }
