90fedabcc5b8c9429809987d80c27a9935207797
Steve <steve.liu.3029@gmail.com>
2019-08-03 13:01:25 +1200

    add animation effect for category nav

--- src/DevYeah.LMS/DevYeah.LMS.Web/app/src/category/CategoryItem.js
+++ src/DevYeah.LMS/DevYeah.LMS.Web/app/src/category/CategoryItem.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function CategoryItem() {
-  
+  const [isHover, setIsHover] = useState(false);
+
   return (
-    <div className="p-2 bg-success cat-item">
+    <div 
+      className={"p-2 cat-item " + (isHover ? " bg-success" : "")}
+      onMouseOver={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
       <a className="cat-nav-link" href="#">
-        <div className="cat-nav-color">
+        <div className={isHover ? "cat-nav-hover-color" : "cat-nav-color"}>
           <i className="far fa-file-code fa-2x"></i>
         </div>
-        <span className="cat-font cat-nav-color">Frontend</span>
+        <span className={"cat-font " + (isHover ? "cat-nav-hover-color" : "cat-nav-color")}>Frontend</span>
       </a>
     </div>
   );
