415b6f5f93b9591da8ae81885edebf4ffce8275a
Steve <steve.liu.3029@gmail.com>
2019-08-08 16:16:21 +1200

    remove rubbish code

--- src/DevYeah.LMS/DevYeah.LMS.Data/LMSContext.cs
+++ src/DevYeah.LMS/DevYeah.LMS.Data/LMSContext.cs
@@ -37,7 +37,7 @@ namespace DevYeah.LMS.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-8A51TKG;Database=LMS;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer("");
             }
         }
 
