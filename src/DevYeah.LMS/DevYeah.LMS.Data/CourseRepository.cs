c246882659a350c0cbbabe0ce7b1c50f4ea8cddd
Steve <steve.liu.3029@gmail.com>
2019-08-10 22:26:52 +1200

    rewrite implementation of querying course list by category

--- src/DevYeah.LMS/DevYeah.LMS.Data/CourseRepository.cs
+++ src/DevYeah.LMS/DevYeah.LMS.Data/CourseRepository.cs
@@ -13,30 +13,13 @@ namespace DevYeah.LMS.Data
         {
         }
 
-        public IEnumerable<Course> GetAllCourses() => FindAll(course => true);
+        public IEnumerable<Course> GetAllCourses() => FindAll(c => true);
 
         public IEnumerable<Course> GetCoursesByCategory(Guid catId)
         {
             var dbCtx = GetDbContext();
-            var coursesFilterByCat = dbCtx.Set<Category>()
-                .Include(t => t.CourseCategory)
-                    .ThenInclude(c => c.Course)
-                        .ThenInclude(c => c.Instructor)
-                .Where(t => t.Id == catId)
-                .AsNoTracking()
-                .FirstOrDefault()
-                .CourseCategory
-                .Select(t => new Course
-                {
-                    Id = t.Course.Id,
-                    Name = t.Course.Name,
-                    InstructorId = t.Course.Instructor.Id,
-                    Overview = t.Course.Overview,
-                    Edition = t.Course.Edition,
-                    Level = t.Course.Level,
-                    AvgLearningTime = t.Course.AvgLearningTime,
-                    ScreenCast = t.Course.ScreenCast
-                })
+            var coursesFilterByCat = dbCtx.Set<Course>()
+                .Where(c => c.CourseCategory.Any(t => t.CategoryId == catId))
                 .ToList();
             return coursesFilterByCat;
         }
